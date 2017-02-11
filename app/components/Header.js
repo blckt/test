a36103760f2128d7397d979c1a137f58debcf95c
Denis Sadilo <Denis Sadilo>
2017-02-11 16:57:43 +0200

    merge

--- app/components/Header.js
+++ app/components/Header.js
@@ -13,20 +13,7 @@ const { app } = require('electron').remote;
 
 
 const Settings = (props) => (
-<<<<<<< HEAD
-  <IconMenu
-    {...props} iconButtonElement={<IconButton > <MoreVertIcon /> </IconButton>} targetOrigin={{
-      horizontal: 'right',
-      vertical: 'top'
-    }} anchorOrigin={{
-      horizontal: 'right',
-      vertical: 'top'
-    }}
-  >
-    <MenuItem primaryText="Refresh" />
-    <MenuItem primaryText="Help" />
-    <MenuItem primaryText="Exit" onClick={props.handleExit} />
-=======
+
   <IconMenu {...props} iconButtonElement={<IconButton> <ExitIcon /> </IconButton>} targetOrigin={{
     horizontal: 'right',
     vertical: 'top'
@@ -35,7 +22,7 @@ const Settings = (props) => (
     vertical: 'top'
   }}>
     <MenuItem primaryText="Exit" leftIcon={<NavigationClose />} onClick={props.handleExit} />
->>>>>>> 0c49053c8d830b1d3fb39752bd0e2b243b0271d7
+
   </IconMenu>
 );
 
@@ -44,41 +31,23 @@ Settings.propTypes = {
 };
 
 export class Header extends Component {
-<<<<<<< HEAD
-  state = {
-    yo: 1
-  }
-  handleChange = (event, logged) => {
-    this.setState({ logged });
-  };
-  handleExit = () => {
-    const x = confirm('Are you sure?');
-=======
+
   constructor(props) {
     super(props);
   }
   handleExit = () => {
     var x = confirm("Are you sure?")
->>>>>>> 0c49053c8d830b1d3fb39752bd0e2b243b0271d7
     if (x) {
       app.exit();
     }
   }
   render() {
     return (
-<<<<<<< HEAD
-      <AppBar
-        title="Capture!" showMenuIconButton={false} iconElementRight={<Settings
-          handleExit={
-            this.handleExit
-          }
-        />}
-      />
-=======
+
       <AppBar style={{ minHeight: '64px' }} title={this.props.title || "App title not provided"} showMenuIconButton={false} iconElementRight={<Settings handleExit={
         this.handleExit
       } />} />
->>>>>>> 0c49053c8d830b1d3fb39752bd0e2b243b0271d7
+
     );
   }
 }
